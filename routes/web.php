a3f7ba13f73e6bffc4810509d580a9cddfc9f80f
Richard <skynet.tw@gmail.com>
2021-01-05 23:03:30 +0800

    fix the slash

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 
 Route::get('/', [MainController::class, "index"]);
-Route::post('/insert/', [MainController::class, "insert"]);
+Route::post('/insert', [MainController::class, "insert"]);
 //Route::get('/insert/', [MainController::class, "insert"]);
-Route::get('/mylogout/', [MainController::class, "logout"]);
+Route::get('/mylogout', [MainController::class, "logout"]);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
