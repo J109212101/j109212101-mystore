e95027491362d56af52275e8665b64e3d95a743b
Richard <skynet.tw@gmail.com>
2021-01-05 22:57:39 +0800

    fix POST to post

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -45,7 +45,7 @@
   </div>
 </nav>
 @auth
-<form method='POST' action="/insert/">
+<form method='post' action="/insert/">
 	@csrf
 最新消息：<input type=text size=40 name=title>
 <input type=submit value="新增">
